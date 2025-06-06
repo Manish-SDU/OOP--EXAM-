56089ac02d6679eaaf443066ebda090ba060b944
Manish-SDU <maraj24@student.sdu.dk>
2025-06-07 00:11:35 +0200

    auto update

--- movielens/Models/Movie.cs
+++
@@ -1,7 +0,0 @@
-public class Movie
-{
-    public int MovieId { get; set; }
-    public string Title { get; set; }
-    public string Genres { get; set; }
-    public List<string> GenresList => Genres.Split('|').ToList();
-}
--- movielens/Models/Rating.cs
+++
@@ -1,7 +0,0 @@
-public class Rating
-{
-    public int MovieId { get; set; }
-    public int UserId { get; set; }
-    public float UserRating { get; set; }
-    public long Timestamp { get; set; }
-}
